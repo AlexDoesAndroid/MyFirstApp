e3485637b5a55fe399da47ed87d9765f36883e6c
doingthisforclass <zam0400@gmail.com>
2021-09-09 09:25:14 -0400

    Initial commit

---
+++ app/src/main/java/edutemple/myfirstapp/MainActivity.kt
@@ -0,0 +1,16 @@
+package edutemple.myfirstapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        //TODO whatever your comment is
+
+    //    val _myTextView findViewByID<TextView>(R.id.myTextView)
+    //    myTextView.SetOnClickListener({it as TextView = "Hello, Alex"})
+
+    }
+}
