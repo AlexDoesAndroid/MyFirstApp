3183876104819b0a7f2c0e3f920d085eb0c6eb8e
doingthisforclass <zam0400@gmail.com>
2021-09-14 14:08:48 -0400

    added button

--- app/src/main/java/edutemple/myfirstapp/MainActivity.kt
+++ app/src/main/java/edutemple/myfirstapp/MainActivity.kt
@@ -1,16 +1,19 @@
 package edutemple.myfirstapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //TODO whatever your comment is
 
-    //    val _myTextView findViewByID<TextView>(R.id.myTextView)
-    //    myTextView.SetOnClickListener({it as TextView = "Hello, Alex"})
+        findViewById<Button>(R.id.button)
+            .setOnClickListener({v, -> (v as Button).text = "You clicked the button"})
 
     }
 }
